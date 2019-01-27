597dce45f2b3aea17fc99b430649d5c4c0cb47dc
William Sena <williampsena@gmail.com>
2019-01-27 17:06:38 -0200

    New version

--- public/ghost-sdk.js
+++ public/ghost-sdk.js
@@ -1,5 +1,5 @@
 (function () {
-        var apiUrl = '//localhost:2368/ghost/api/v0.1/',
+        var apiUrl = '//www.coisadeprogramador.com.br/ghost/api/v0.1/',
         clientId,
         clientSecret,
         url,
